9a942e51f82da2b3aed3030fe35045990837c7b5
Unknown <neto3333@gmail.com>
2020-04-25 07:28:04 -0400

    Criado projeto e feito algumas coisas.

---
+++ desafio-ios-ruy-neto/desafio-ios-ruy-neto/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  desafio-ios-ruy-neto
+//
+//  Created by Ruy de Ascencão Neto on 25/04/20.
+//  Copyright © 2020 Ruy Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tabela: UITableView!
+    
+    let lista  = Array(1...101)
+    var limite = 20
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tabela.delegate   = self
+        tabela.dataSource = self
+    }
+
+}
+
+extension ViewController:UITableViewDelegate{
+
+}
+
+extension ViewController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.limite
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(lista[indexPath.row])"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == self.limite {
+            print(((self.limite + 20) < (lista.count - 1)) ? 20:(lista.count - 1 - self.limite))
+            self.limite += ((self.limite + 20) < (lista.count - 1)) ? 20:(lista.count - 1 - self.limite)
+            self.tabela.reloadData()
+        }
+    }
+    
+
+}
---
+++ desafio-ios-ruy-neto/desafio-ios-ruy-netoTests/desafio_ios_ruy_netoTests.swift
@@ -0,0 +1,34 @@
+//
+//  desafio_ios_ruy_netoTests.swift
+//  desafio-ios-ruy-netoTests
+//
+//  Created by Ruy de Ascencão Neto on 25/04/20.
+//  Copyright © 2020 Ruy Neto. All rights reserved.
+//
+
+import XCTest
+@testable import desafio_ios_ruy_neto
+
+class desafio_ios_ruy_netoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
